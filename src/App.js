c572732c53b6ec91db9cbd56905ff9e7269cdc25
nachan28 <torajiro282828@gmail.com>
2023-02-09 20:24:14 +0900

    add: add functions that fetch poke API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,49 @@
 import "./App.css";
+import { useEffect, useState } from "react";
+import { getAllPokemon, getPokemon } from "./utils/pokemon.js";
+import Card from "./components/Card";
 
 function App() {
-  return <div className="App"></div>;
+  const initialURL = "https://pokeapi.co/api/v2/pokemon";
+  const [loading, setLoading] = useState(true);
+  const [pokemonData, setPokemonData] = useState([]);
+  const loadPokemon = async (data) => {
+    let _pokemonData = await Promise.all(
+      data.map((res) => {
+        let pokemonRecord = getPokemon(res.url);
+        return pokemonRecord;
+      })
+    );
+    setPokemonData(_pokemonData);
+  };
+
+  useEffect(() => {
+    const fetchPokemonData = async (url) => {
+      let res = await getAllPokemon(url);
+      loadPokemon(res.results);
+      console.log(res);
+      setLoading(false);
+    };
+
+    fetchPokemonData(initialURL);
+  }, []);
+
+  console.log(pokemonData);
+  return (
+    <div className="App">
+      {loading ? (
+        <h1>ロード中...</h1>
+      ) : (
+        <>
+          <div className="pokeomnCardContainer">
+            {pokemonData.map((pokemon, i) => {
+              return <Card key={i} pokemon={pokemon}/>;
+            })}
+          </div>
+        </>
+      )}
+    </div>
+  );
 }
 
 export default App;
